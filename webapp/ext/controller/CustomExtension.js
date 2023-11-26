17554d7c81a1f40fcefad3f6292372d2b047a3ec
Kwang-Hoon Han <kh.han@readians.com>
2023-11-26 16:21:11 +0000

    20231127 박원종

--- webapp/ext/controller/CustomExtension.js
+++ webapp/ext/controller/CustomExtension.js
@@ -9,6 +9,29 @@ sap.ui.define([
         },
         CreateData: function(oEvent){
             this.getRouting().navigateToRoute('CreateTemplate',this);
+        },
+        CancelData: function(oEvent){
+            let oTable = this.oView.byId('fi.zfrfi0050::ZFI_C_OTHER_RECEIPTList--fe::table::ZFI_C_OTHER_RECEIPT::LineItem-innerTable');
+            oBindContext.requestObject().then(function(oResult){
+                let a = oResult;
+            }.bind(this));
+
+            const headers = {
+                'X-Csrf-Token' : this.getModel().getHttpHeaders()['X-CSRF-Token']
+            };
+            console.log(this.getModel().getHttpHeaders());
+            ///sap/opu/odata4/sap/zfi_c_other_receipt_ui_v4/srvd/sap/zfi_c_other_receipt_ui/0001/ZFI_C_DOC_APPROVAL/com.sap.gateway.srvd.zfi_c_other_receipt_ui.v0001.Posting
+            axios.post('/sap/opu/odata4/sap/zfi_u_tran_approval_ui_v4/srvd/sap/zfi_u_tran_approval_ui/0001/ZFI_IR_TRAN_APPROVAL/com.sap.gateway.srvd.zfi_u_tran_approval_ui.v0001.CancelReq', 
+                {
+              },{
+                headers : headers
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
         }
     };
 });
