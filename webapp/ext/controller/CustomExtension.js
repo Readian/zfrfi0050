00dba947df68e046d81529034bb18782459db84e
Kwang-Hoon Han <kh.han@readians.com>
2023-11-28 05:47:05 +0000

    20231128 박원종

--- webapp/ext/controller/CustomExtension.js
+++ webapp/ext/controller/CustomExtension.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-    "sap/m/MessageToast"
-], function(MessageToast) {
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+], function(
+        MessageToast,
+        MessageBox) {
     'use strict';
 
     return {
@@ -11,27 +14,37 @@ sap.ui.define([
             this.getRouting().navigateToRoute('CreateTemplate',this);
         },
         CancelData: function(oEvent){
-            let oTable = this.oView.byId('fi.zfrfi0050::ZFI_C_OTHER_RECEIPTList--fe::table::ZFI_C_OTHER_RECEIPT::LineItem-innerTable');
-            oBindContext.requestObject().then(function(oResult){
-                let a = oResult;
-            }.bind(this));
+            let oTable = this._view.byId('fi.zfrfi0050::ZFI_C_OTHER_RECEIPTList--fe::table::ZFI_C_OTHER_RECEIPT::LineItem-innerTable');
+            let oSelectedItem = oTable.getSelectedItems();
 
-            const headers = {
-                'X-Csrf-Token' : this.getModel().getHttpHeaders()['X-CSRF-Token']
-            };
-            console.log(this.getModel().getHttpHeaders());
-            ///sap/opu/odata4/sap/zfi_c_other_receipt_ui_v4/srvd/sap/zfi_c_other_receipt_ui/0001/ZFI_C_DOC_APPROVAL/com.sap.gateway.srvd.zfi_c_other_receipt_ui.v0001.Posting
-            axios.post('/sap/opu/odata4/sap/zfi_u_tran_approval_ui_v4/srvd/sap/zfi_u_tran_approval_ui/0001/ZFI_IR_TRAN_APPROVAL/com.sap.gateway.srvd.zfi_u_tran_approval_ui.v0001.CancelReq', 
-                {
-              },{
-                headers : headers
-              })
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+            MessageBox.confirm("상신을 취소하시겠습니까?",{
+                actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+                onClose: function(oAction){
+                    if(oAction == sap.m.MessageBox.Action.YES)
+                    {
+
+                        const headers = {
+                            'X-Csrf-Token' : this.getModel().getHttpHeaders()['X-CSRF-Token']
+                        };
+                        console.log(this.getModel().getHttpHeaders());
+                        ///sap/opu/odata4/sap/zfi_c_other_receipt_ui_v4/srvd/sap/zfi_c_other_receipt_ui/0001/ZFI_C_DOC_APPROVAL/com.sap.gateway.srvd.zfi_c_other_receipt_ui.v0001.Posting
+                        axios.post('/sap/opu/odata4/sap/zfi_c_other_receipt_ui_v4/srvd/sap/zfi_c_other_receipt_ui/0001/ZFI_C_DOC_APPROVAL/com.sap.gateway.srvd.zfi_c_other_receipt_ui.v0001.CancelReq', 
+                            {
+                          },{
+                            headers : headers
+                          })
+                          .then(function (response) {
+                            console.log(response);
+                          })
+                          .catch(function (error) {
+                            console.log(error);
+                          });
+                    }
+                }.bind(this)
+            })
+
+
+            
         }
     };
 });
